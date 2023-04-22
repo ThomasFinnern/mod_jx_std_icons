42ec18dd53906d29abcc93ed3000703dd199ada2
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2023-04-22 20:56:06 +0200

    Display icon in table form

--- tmpl/default.php
+++ tmpl/default.php
@@ -14,24 +14,22 @@ use Joomla\CMS\Language\Text;
 
 global $test, $icons, $defaultIcons;
 
-
-HTMLHelper::_('stylesheet', 'mod_j4_standard_icons/css/template.css', array('version' => 'auto', 'relative' => true));
-//$wa = $app->getDocument()->getWebAssetManager();
-//$wa->registerAndUseStyle('mod_j4_standard_icons', 'mod_j4_standard_icons/template.css');
-
-// $app->getDocument()->getWebAssetManager()
-	// ->useScript('core')
-	// ->useScript('keepalive')
-	// ->useScript('field.passwordview');
-
-// $wa->registerAndUseStyle('mod_languages', 'mod_languages/template.css');
+// load
+$wa = $app->getDocument()->getWebAssetManager();
+$wa->registerAndUseStyle('mod_j4_std_icons', 'mod_j4_std_icons/template.css');
 
 ?>
-<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'defaulticons', Text::_('COM_ADMIN_AVAILABLE_ICONS')); ?>
+
+<?php //echo HTMLHelper::_('uitab.addTab', 'myTab', 'defaulticons', Text::_('COM_ADMIN_AVAILABLE_ICONS')); ?>
 <?php // echo $this->loadTemplate('defaulticons'); ?>
 
 
-echo '[J!4 Standard Icons] ' . $test . '<br />'; // . $url;
+<?php
+
+	echo '<div class="test01">';
+	echo '[J!4 Standard Icons] ' . $test . '<br />'; // . $url;
+	echo '</div">';
+?>
 
 
 
@@ -41,6 +39,19 @@ if (empty($icons)) {
 
     return;
 }
+
+$icomoons = [];
+
+foreach ($icons as $iconName => $iconSet) {
+
+	if ($iconSet[0]->iconType == '.icon') {
+
+		$icomoons [] = $iconSet[0]->name;
+	}
+}
+
+
+$icons = [];
 
 foreach ($icons as $iconName => $iconSet) {
 
@@ -110,48 +121,109 @@ foreach ($icons as $iconName => $iconSet) {
 
 <?php } ?>
 
-<div class="card mb-3 ">
-	<div class="card-header">
-		<h2>
-			<span class="icon-joomla" aria-hidden="true"></span>
-			<?php echo Text::_('COM_ADMIN_AVAILABLE_ICONS'); ?>
-		</h2>
-	</div>
+
+<div class="card" >
 	<div class="card-body">
-		<nav class="quick-icons px-3 pb-3">
-			<ul class="nav flex-wrap">
-				<li class="quickicon quickicon-single">
-					<a href="#">
-						<div class="quickicon-info">
-							<div class="quickicon-icon">
-								<span class="icon-joomla" style="font-size: 48px;" aria-hidden="true"></span>
-							</div>
-						</div>
-						<div class="quickicon-name d-flex align-items-end">
-							Joomla
-						</div>
-					</a>
-				</li>
+<!--		ToDo: User Flag for title -->
+		<h2 class="card-title"><?php echo Text::_('MOD_J4_STD_ICONS_AVAILABLE_ICONS'); ?></h2>
+		<h6 class="card-subtitle mb-2 text-muted"><?php echo Text::_('MOD_J4_STD_ICONS_AVAILABLE_ICONS_DESC'); ?></h6>
+		<br>
 
-				<?php foreach ($defaultIcons as $item): ?>
-					<li class="quickicon quickicon-single">
-						<a href="#">
-							<div class="quickicon-info">
-								<div class="quickicon-icon">
-									<i style="font-size: 48px;" class="fa fa-<?php echo $item; ?>"></i>
+		<!-- icomoon replacements ======================================================================== -->
+		<div class="card mb-3 ">
+			<div class="card-header">
+				<h2>
+					<span class="icon-joomla" aria-hidden="true"></span>
+					<?php echo Text::_('MOD_J4_STD_ICONS_ICOMOON_ICONS'); ?>
+				</h2>
+			</div>
 
+			<div class="card-body">
+				<h6 class="card-title"><?php echo Text::_('MOD_J4_STD_ICONS_ICOMOON_ICONS_DESC'); ?></h6>
+				<nav class="quick-icons px-3 pb-3">
+					<ul class="nav flex-wrap">
+						<li class="quickicon quickicon-single">
+							<a href="#">
+								<div class="quickicon-info">
+									<div class="quickicon-icon">
+										<span class="icon-joomla" style="font-size: 48px;" aria-hidden="true"></span>
+									</div>
 								</div>
-							</div>
-							<div class="quickicon-name d-flex align-items-end">
-								<?php echo $item; ?>
-							</div>
-						</a>
-					</li>
-				<?php endforeach; ?>
-			</ul>
-		</nav>
-	</div>
+								<div class="quickicon-name d-flex align-items-end">
+									Joomla
+								</div>
+							</a>
+						</li>
+
+						<?php foreach ($icomoons as $item): ?>
+							<li class="quickicon quickicon-single">
+								<a href="#">
+									<div class="quickicon-info">
+										<div class="quickicon-icon">
+											<i style="font-size: 48px;" class="icon-<?php echo $item; ?>"></i>
+
+										</div>
+									</div>
+									<div class="quickicon-name d-flex align-items-end">
+										<?php echo $item; ?>
+									</div>
+								</a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</nav>
+
+		</div>
+
+		<!-- font awesome ======================================================================== -->
+		<div class="card mb-3 ">
+			<div class="card-header">
+				<h2>
+					<span class="icon-joomla" aria-hidden="true"></span>
+					<?php echo Text::_('MOD_J4_STD_ICONS_AWESOME_ICONS'); ?>
+				</h2>
+			</div>
+			<div class="card-body">
+				<h6 class="card-title"><?php echo Text::_('MOD_J4_STD_ICONS_AWESOME_ICONS_DESC'); ?></h6>
+				<nav class="quick-icons px-3 pb-3">
+					<ul class="nav flex-wrap">
+						<li class="quickicon quickicon-single">
+							<a href="#">
+								<div class="quickicon-info">
+									<div class="quickicon-icon">
+										<span class="icon-joomla" style="font-size: 48px;" aria-hidden="true"></span>
+									</div>
+								</div>
+								<div class="quickicon-name d-flex align-items-end">
+									Joomla
+								</div>
+							</a>
+						</li>
+
+						<?php foreach ($defaultIcons as $item): ?>
+							<li class="quickicon quickicon-single">
+								<a href="#">
+									<div class="quickicon-info">
+										<div class="quickicon-icon">
+											<i style="font-size: 48px;" class="fa fa-<?php echo $item; ?>"></i>
+
+										</div>
+									</div>
+									<div class="quickicon-name d-flex align-items-end">
+										<?php echo $item; ?>
+									</div>
+								</a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</nav>
+			</div>
+		</div>
+
+</div>
 </div>
 
-<?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+
+<?php //echo HTMLHelper::_('uitab.endTab'); ?>
 
