b259412b4ccf34d8b2ee659c219aaace1a0f8598
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2023-06-05 22:59:09 +0200

    test brand icons

--- tmpl/default.php
+++ tmpl/default.php
@@ -22,6 +22,7 @@ $isDisplayTablesHeader = $params->get('isDisplayTablesHeader');
 $isDisplayTechDetail = $params->get('isDisplayTechDetail');
 $isDisplayIconTable_Icon = $params->get('isDisplayIconTable_Icon');
 $isDisplayIconTable_Awesome = $params->get('isDisplayIconTable_Awesome');
+$isDisplayBrandIconsTable_Awesome = $params->get('isDisplayBrandIconsTable_Awesome');
 
 $font_size = $params->get('font_size');
 
@@ -108,7 +109,7 @@ $font_size = $params->get('font_size');
 				</div>
 			<?php endif; ?>
 
-			<!-- font awesome ======================================================================== -->
+			<!-- font awesome no brands ======================================================================== -->
 
 			<?php if($isDisplayIconTable_Awesome): ?>
 				<div class="card mb-3 ">
@@ -165,10 +166,73 @@ $font_size = $params->get('font_size');
 							</ul>
 						</nav>
 						<h5>Count: <span class="badge bg-secondary"><?php echo count ($svg_icons); ?></span></h5>
+
 					</div>
 				</div>
 			<?php endif; ?>
 
+			<!-- font awesome brands only ======================================================================== -->
+
+            <?php if($isDisplayBrandIconsTable_Awesome): ?>
+
+				<div class="card mb-3 ">
+					<div class="card-header">
+						<h2>
+							<span class="icon-joomla" aria-hidden="true"></span>
+							<?php echo Text::_('MOD_J4_STD_ICONS_AWESOME_BRAND_ICONS'); ?>
+						</h2>
+					</div>
+					<div class="card-body">
+
+                        <?php if($isDisplayTechDetail): ?>
+                            <div class="mb-3">
+                                <div class="card-title"><?php echo Text::_('MOD_J4_STD_ICONS_AWESOME_BRAND_ICONS_DESC'); ?></div>
+                            </div>
+                        <?php endif; ?>
+
+                        <nav class="quick-icons px-3 pb-3">
+                            <ul class="nav flex-wrap">
+                                <li class="quickicon quickicon-single">
+                                    <a href="#">
+                                        <div class="quickicon-info">
+                                            <div class="quickicon-icon">
+                                                <span class="icon-joomla" style="font-size: <?php echo $font_size; ?>px;" aria-hidden="true"></span>
+                                            </div>
+                                        </div>
+                                        <div class="quickicon-name d-flex align-items-end">
+                                            Joomla
+                                        </div>
+                                    </a>
+                                </li>
+
+                                <?php foreach ($svg_brand_icons as $item): ?>
+                                    <li class="quickicon quickicon-single">
+                                        <a href="#">
+                                            <div class="quickicon-info">
+                                                <div class="quickicon-icon">
+                                                    <i style="font-size: <?php echo $font_size; ?>px;" class="fa fa-<?php echo $item; ?>"></i>
+
+                                                </div>
+                                            </div>
+                                            <div class="quickicon-name d-flex align-items-end">
+                                                <?php if ( in_array ($item, $j4x_css_awesome_icons)): ?>
+                                                    <strong><?php echo $item; ?></strong>
+                                                <?php else: ?>
+                                                    <del>
+                                                    <?php echo $item; ?>
+                                                    </del>
+                                                <?php endif ?>
+                                            </div>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </nav>
+                        <h5>Count: <span class="badge bg-secondary"><?php echo count ($svg_brand_icons); ?></span></h5>
+
+					</div>
+				</div>
+            <?php endif; ?>
 		<?php endif; ?>
 	</div>
 </div>
