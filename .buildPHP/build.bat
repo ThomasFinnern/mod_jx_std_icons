db9f2193aeb72feb2370097bad6d9c4f2f221ee1
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2025-04-02 20:57:01 +0200

    removed not needed build.bats

--- .buildPHP/build.bat
+++ .buildPHP/build.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
-REM build_step.bat
-REM like build_develop but does increase minor 'build' number 
+REM build.bat
+REM options file can be selected 
+REM d:develop, s:step, f:fix, r:release, m:major
 CLS
 
 REM Path for calling
@@ -22,12 +23,37 @@ REM "C:\Program Files\php82\php.exe" --version
 ECHO ----------------------------------------------
 ECHO.
 
+set OptionFile=
+
+if %1A==-dA (
+	set OptionFile=-o options_version_tsk\build_develop.opt
+)
+
+if %1A==-sA (
+	set OptionFile=-o options_version_tsk\build_step.opt
+)
+
+if %1A==-fA (
+	set OptionFile=-o options_version_tsk\build_fix.opt
+)
+
+if %1A==-rA (
+	set OptionFile=-o options_version_tsk\build_release.opt
+)
+
+if %1A==-mA (
+	set OptionFile=-o options_version_tsk\build_major.opt
+)
+
+
+ECHO ----------------------------------------------
+ECHO.
+
 pushd  ..\..\buildExtension\src
-REM dir /one /b *.tsk
 ECHO Path: %cd% 
 
-echo --- "%ExePath%php.exe" buildExtensionCmd.php -f ../../mod_jx_std_icons/.buildPHP/build_step.tsk %1
-"%ExePath%php.exe" buildExtensionCmd.php -f ../../mod_jx_std_icons/.buildPHP/build_step.tsk %1
+echo --- "%ExePath%php.exe" buildExtensionCmd.php -f ../../mod_jx_std_icons/.buildPHP/build.tsk %OptionFile%
+"%ExePath%php.exe" buildExtensionCmd.php -f ../../mod_jx_std_icons/.buildPHP/build.tsk %OptionFile%
 popd
 
 GOTO :EOF
